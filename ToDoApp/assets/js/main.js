f1a5bf542a16e2ad6a57d338d9f0dc6945a9e198
Shubham Dobriyal <shubhof7@gmail.com>
2020-01-16 21:18:58 +0530

    create main.js

---
+++ ToDoApp/assets/js/main.js
@@ -0,0 +1,30 @@
+$('.todo-list').on('click','li',function(){
+    $(this).toggleClass('todo_done');
+}); 
+
+$('.todo-list').on('click','li span',function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    })
+    e.stopPropagation();
+})
+
+
+$('.todo-form input').keypress(function (e) { 
+    if(e.which === 13){
+        var todoText = $(this).val();
+        $(this).val('');
+        $('.todo-list ul').append('<li><span class="delete-btn"><i class="fas fa-trash-alt"></i></span> '+ todoText +'</li>');
+    }
+});
+
+$('.form-toggle-icon').click(function(){
+    $('.todo-form').fadeToggle();
+    if($('.form-toggle-icon i').hasClass('fa-plus')){
+        $('.form-toggle-icon i').removeClass('fa-plus');
+        $('.form-toggle-icon i').addClass('fa-minus');
+    }else{
+        $('.form-toggle-icon i').addClass('fa-plus');
+        $('.form-toggle-icon i').removeClass('fa-minus');
+    }
+});
